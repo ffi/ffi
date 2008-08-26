2bbc8cec97cb5a0f3477d0bbc9916458a56cb845
wmeissner <devnull@localhost>
2008-08-26 23:06:50 +1000

    Append rules to the main Makefile to build libffi

--- extconf.rb
+++ extconf.rb
@@ -1,3 +1,9 @@
+#!/usr/bin/env ruby
 require 'mkmf'
 dir_config("ffi")
 create_makefile("ffi")
+File.open("Makefile", "a") do |mf|
+  mf.puts "build_libffi:\n"
+  mf.puts "\t$(MAKE) -f Makefile.libffi"
+  mf.puts "$(DLLIB):\tbuild_libffi"
+end
