03eab1d8853123f47d9bbba50de2bf5d54df0cca
Wayne Meissner <wmeissner@gmail.com>
2008-09-13 17:17:48 +1000

    move libffi and libffi.mk to ext/

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -6,7 +6,7 @@ File.open("Makefile", "a") do |mf|
   mf.puts "OS=$(shell uname -s)"
   mf.puts "ARCH=$(shell uname -p)"
   mf.puts 'CFLAGS += -DOS=\\"$(OS)\\" -DARCH=\\"$(ARCH)\\"'
-  mf.puts "include libffi.mk"
+  mf.puts "include $(srcdir)/libffi.mk"
   mf.puts "$(OBJS):\t$(LIBFFI)"
   mf.puts "LOCAL_LIBS += $(LIBFFI)"
   mf.puts "clean:\tlibffi_clean"
