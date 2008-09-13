ee6d9d9df4b23465ae8043cde041ba4e3af1e628
wmeissner <devnull@localhost>
2008-09-13 15:22:01 +1000

    Expose the OS via bsd?, windows?, unix?, linux?, solaris?, mac?, etc methods

--- lib/ffi/platform.rb
+++ lib/ffi/platform.rb
@@ -4,7 +4,7 @@ module FFI
     def self.is_os(os)
       OS =~ /#{os}/ ? true : false
     end
-    public
+    
     NAME = "#{ARCH}-#{OS}"
     IS_LINUX = is_os("linux")
     IS_MAC = is_os("darwin")
@@ -12,6 +12,7 @@ module FFI
     IS_OPENBSD = is_os("windows")
     IS_WINDOWS = is_os("windows") || is_os("win32")
     IS_BSD = IS_MAC || IS_FREEBSD || IS_OPENBSD
+    public
     LIBC = IS_WINDOWS ? "msvcrt" : "c"
     LIBPREFIX = IS_WINDOWS ? '' : 'lib'
     LIBSUFFIX = case OS
@@ -19,7 +20,20 @@ module FFI
       '.dylib'
     when /(linux|.*bsd)/
       '.so'
+    when /win/
+      '.dll'
     end
-    
+    def self.bsd?
+      IS_BSD
+    end
+    def self.windows?
+      IS_WINDOWS
+    end
+    def self.mac?
+      IS_MAC
+    end
+    def self.unix?
+      !IS_WINDOWS
+    end
   end
 end
