74f622f51980858c5a884679e3eaf9facc592c0e
Wayne Meissner <wmeissner@gmail.com>
2008-09-13 17:54:34 +1000

    Don't require ffi.so

--- lib/ffi/ffi.rb
+++ lib/ffi/ffi.rb
@@ -51,7 +51,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-require 'ffi.so'
+#require 'ffi.so'
 require 'ffi/platform'
 require 'ffi/memorypointer'
 require 'ffi/struct'
