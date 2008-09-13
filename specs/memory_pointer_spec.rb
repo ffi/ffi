349f86256bd01cd7741aad809bb6f90a9c06d7f7
wycats@gmail.com <wycats@gmail.com>
2008-09-13 02:38:15 -0700

    Some more MemoryPointer specs

--- specs/memory_pointer_spec.rb
+++ specs/memory_pointer_spec.rb
@@ -19,6 +19,35 @@ describe MemoryPointer do
     m.write_array_of_int([1,2])
     m.read_array_of_int(2).should == [1,2]
   end
+
+  it "allows access to an element of the pointer (as an array)" do
+    m = MemoryPointer.new(8)
+    m.write_array_of_int([1,2])
+    m[0].should == 1
+    m[1].should == 2
+  end
+  
+  it "allows writing as an int" do
+    m = MemoryPointer.new(:int)
+    m.write_int(1)
+    m.read_int.should == 1
+  end
+  
+  it "allows writing as a long" do
+    m = MemoryPointer.new(:long)
+    m.write_long(10)
+    m.read_long.should == 10
+  end
+  
+  it "raises an error if you try putting a long into a pointer of size 1" do
+    m = MemoryPointer.new(1)
+    lambda { m.write_long(10) }.should raise_error
+  end
+  
+  it "raises an error if you try putting an int into a pointer of size 1" do
+    m = MemoryPointer.new(1)
+    lambda { m.write_int(10) }.should raise_error
+  end
   
   it "makes a pointer for a certain type" do
     m = MemoryPointer.new(:int)
