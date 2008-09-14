fe47d7200b06d41de81c4dfb52fc3f8a3c491ec1
Wayne Meissner <wmeissner@gmail.com>
2008-09-14 13:42:01 +1000

    Cleanup libffi missing files caused by over-zealous 'Makefile' line in .hgignore

---
+++ nbproject/Makefile-Default.mk
@@ -0,0 +1,54 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=
+
+# Include project Makefile
+include ruby-ffi-Makefile.mk
+
+# Object Directory
+OBJECTDIR=build/Default/GNU-MacOSX
+
+# Object Files
+OBJECTFILES=
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	cd . && make -f Makefile
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf:
+	cd . && make -f Makefile clean
+
+# Subprojects
+.clean-subprojects:
