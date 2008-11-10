b8903b6dd5c5eaa82483345a15fcbb25014d8afd
Wayne Meissner <wmeissner@gmail.com>
2008-11-10 17:18:23 +1000

    Implement FFI.last_error

--- ext/Invoker.c
+++ ext/Invoker.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <dlfcn.h>
+#include <errno.h>
 #include <ruby.h>
 
 #include <ffi.h>
@@ -28,7 +29,9 @@ typedef struct Invoker {
     VALUE* callbackParameters;
     ffi_abi abi;
 } Invoker;
-
+typedef struct ThreadData {
+    int td_errno;
+} ThreadData;
 static VALUE invoker_new(VALUE self, VALUE library, VALUE function, VALUE parameterTypes,
         VALUE returnType, VALUE convention);
 static void invoker_mark(Invoker *);
@@ -37,8 +40,12 @@ static VALUE invoker_call(int argc, VALUE* argv, VALUE self);
 static VALUE invoker_call0(VALUE self);
 static VALUE invoker_arity(VALUE self);
 static void* callback_param(VALUE proc, VALUE cbinfo);
+static inline ThreadData* thread_data_get(void);
 static VALUE classInvoker = Qnil, classVariadicInvoker = Qnil;
 static ID cbTableID, to_ptr;
+static pthread_key_t threadDataKey;
+
+#define threadData (thread_data_get())
 
 static VALUE
 invoker_new(VALUE klass, VALUE library, VALUE function, VALUE parameterTypes,
@@ -266,6 +273,7 @@ invoker_call(int argc, VALUE* argv, VALUE self)
     Data_Get_Struct(self, Invoker, invoker);
     ffi_arg_setup(invoker, argc, argv, invoker->paramTypes, params, ffiValues);
     ffi_call(&invoker->cif, FFI_FN(invoker->function), &retval, ffiValues);
+    threadData->td_errno = errno;
     return rb_FFI_NativeValueToRuby(invoker->returnType, &retval);
 }
 
@@ -278,6 +286,7 @@ invoker_call0(VALUE self)
     
     Data_Get_Struct(self, Invoker, invoker);
     ffi_call(&invoker->cif, FFI_FN(invoker->function), &retval, ffiValues);
+    threadData->td_errno = errno;
     return rb_FFI_NativeValueToRuby(invoker->returnType, &retval);
 }
 
@@ -291,6 +300,7 @@ invoker_call1(VALUE self, VALUE arg1)
     Data_Get_Struct(self, Invoker, invoker);
     ffi_arg_setup(invoker, 1, &arg1, invoker->paramTypes, params, ffiValues);
     ffi_call(&invoker->cif, FFI_FN(invoker->function), &retval, ffiValues);
+    threadData->td_errno = errno;
     return rb_FFI_NativeValueToRuby(invoker->returnType, &retval);
 }
 
@@ -305,6 +315,7 @@ invoker_call2(VALUE self, VALUE arg1, VALUE arg2)
     Data_Get_Struct(self, Invoker, invoker);
     ffi_arg_setup(invoker, 2, argv, invoker->paramTypes, params, ffiValues);
     ffi_call(&invoker->cif, FFI_FN(invoker->function), &retval, ffiValues);
+    threadData->td_errno = errno;
     return rb_FFI_NativeValueToRuby(invoker->returnType, &retval);
 }
 
@@ -319,6 +330,7 @@ invoker_call3(VALUE self, VALUE arg1, VALUE arg2, VALUE arg3)
     Data_Get_Struct(self, Invoker, invoker);
     ffi_arg_setup(invoker, 3, argv, invoker->paramTypes, params, ffiValues);
     ffi_call(&invoker->cif, FFI_FN(invoker->function), &retval, ffiValues);
+    threadData->td_errno = errno;
     return rb_FFI_NativeValueToRuby(invoker->returnType, &retval);
 }
 
@@ -414,6 +426,7 @@ variadic_invoker_call(VALUE self, VALUE parameterTypes, VALUE parameterValues)
     }
     ffi_arg_setup(invoker, paramCount, argv, paramTypes, params, ffiValues);
     ffi_call(&cif, FFI_FN(invoker->function), &retval, ffiValues);
+    threadData->td_errno = errno;
     return rb_FFI_NativeValueToRuby(invoker->returnType, &retval);
 }
 
@@ -434,6 +447,38 @@ callback_param(VALUE proc, VALUE cbInfo)
     rb_hash_aset(cbTable, cbInfo, callback);
     return ((NativeCallback *) DATA_PTR(callback))->code;
 }
+#if RUBY_VERSION_CODE >= 190
+static inline ThreadData*
+thread_data_get()
+{
+    ThreadData* td = pthread_getspecific(threadDataKey);
+    if (td == null) {
+        td = ALLOC_N(ThreadData, 1);
+        memset(td, 0, sizeof(*td));
+        pthread_setspecific(threadDataKey, td);
+    }
+    return  td;
+}
+
+static void
+thread_data_free(void *ptr)
+{
+    xfree(ptr);
+}
+#else /* ruby version < 1.9.0 */
+static inline ThreadData*
+thread_data_get()
+{
+    static ThreadData td0;
+    return &td0;
+}
+#endif
+static VALUE
+last_error(VALUE self)
+{
+    return INT2FIX(threadData->td_errno);
+}
+
 void 
 rb_FFI_Invoker_Init()
 {
@@ -451,4 +496,8 @@ rb_FFI_Invoker_Init()
     rb_define_method(classVariadicInvoker, "invoke", variadic_invoker_call, 2);
     cbTableID = rb_intern("__ffi_callback_table__");
     to_ptr = rb_intern("to_ptr");
+    rb_define_module_function(moduleFFI, "last_error", last_error, 0);
+#if RUBY_VERSION_CODE >= 190
+    pthread_key_create(&threadDatakey, thread_data_free);
+#endif
 }
