4de32fb4812907bf3e7a778e1d266dd7b506f4f5
Wayne Meissner <wmeissner@gmail.com>
2008-12-05 09:38:11 +1000

    Add support for autotest.  Patch from Andrea Fazzi <andrea.fazzi@alcacoop.it>

--- spec/ffi/spec_helper.rb
+++ spec/ffi/spec_helper.rb
@@ -1,5 +1,9 @@
-$:.unshift File.join(File.dirname(__FILE__), "..", "lib") if ENV["MRI_FFI"]
+require 'rubygems'
+require 'spec'
+
+$:.unshift File.join(File.dirname(__FILE__), "../..", "lib"), File.join(File.dirname(__FILE__), "../..", "build") if ENV["MRI_FFI"]
 require "ffi"
+
 module TestLibrary
   PATH = "#{Dir.getwd}/build/libtest.#{FFI::Platform::LIBSUFFIX}"
 end
