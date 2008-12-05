b4187d1c893c9a71567f14f13d40e6a36b61a5b6
Wayne Meissner <wmeissner@gmail.com>
2008-12-05 22:09:32 +1000

    Add Invoker#call0 getuid benchmark

--- bench/bench_getuid.rb
+++ bench/bench_getuid.rb
@@ -17,6 +17,14 @@ puts "Benchmark FFI getuid performance, #{iter}x calls"
     iter.times { Posix.getuid }
   }
 }
+puts "Benchmark FFI Invoker#call0() getuid performance, #{iter}x calls"
+invoker = FFI.create_invoker(nil, 'getuid', [], :uint)
+10.times {
+  puts Benchmark.measure {
+    iter.times { invoker.call0() }
+  }
+}
+
 puts "Benchmark Process.uid performance, #{iter}x calls"
 10.times {
   puts Benchmark.measure {
