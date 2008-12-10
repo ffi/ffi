334ab34177bd6a2727380901a5de13c6b3b25603
Wayne Meissner <wmeissner@gmail.com>
2008-12-11 08:21:50 +1000

    Abstract the dl_* functions so there isn't #ifdef _WIN32 littered everywhere

--- ext/ffi_c/NativeLibrary.c
+++ ext/ffi_c/NativeLibrary.c
@@ -18,6 +18,19 @@
 static void library_free(Library* lib);
 static VALUE classLibrary;
 
+#ifdef _WIN32
+static void* dl_open(const char* name, int flags);
+static void dl_error(char* buf, int size);
+#define dl_sym(handle, name) GetProcAddress(handle, name)
+#define dl_close(handle) FreeLibrary(handle)
+enum { RTLD_LAZY=1, RTLD_NOW, RTLD_GLOBAL, RTLD_LOCAL };
+#else
+#define dl_open(name, flags) dlopen(name, flags != 0 ? flags : RTLD_LAZY)
+#define dl_error(buf, size) do { snprintf(buf, size, "%s", dlerror()); } while(0)
+#define dl_sym(handle, name) dlsym(handle, name)
+#define dl_close(handle) dlclose(handle)
+#endif
+
 static VALUE
 library_open(VALUE klass, VALUE libname, VALUE libflags)
 {
@@ -28,25 +41,12 @@ library_open(VALUE klass, VALUE libname, VALUE libflags)
     Check_Type(libflags, T_FIXNUM);
 
     retval = Data_Make_Struct(klass, Library, NULL, library_free, library);
-#ifdef _WIN32
-    library->handle = LoadLibraryEx(StringValueCStr(libname), NULL,
-            LOAD_WITH_ALTERED_SEARCH_PATH);
-#else
     flags = libflags != Qnil ? NUM2UINT(libflags) : 0;
-    if (flags == 0) {
-        flags = RTLD_LAZY;
-    }
-
-    library->handle = dlopen(libname != Qnil ? StringValueCStr(libname) : NULL, flags);
-#endif
+    
+    library->handle = dl_open(libname != Qnil ? StringValueCStr(libname) : NULL, flags);
     if (library->handle == NULL) {
         char errmsg[1024];
-#ifdef _WIN32
-        FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(),
-                0, errmsg, sizeof(errmsg), NULL);
-#else
-        snprintf(errmsg, sizeof(errmsg), "%s", dlerror());
-#endif
+        dl_error(errmsg, sizeof(errmsg));
         rb_raise(rb_eLoadError, "Could not open library '%s': %s",
                 libname != Qnil ? StringValueCStr(libname) : "[current process]",
                 errmsg);
@@ -63,25 +63,16 @@ library_dlsym(VALUE self, VALUE name)
     Check_Type(name, T_STRING);
 
     Data_Get_Struct(self, Library, library);
-#ifdef _WIN32
-    address = GetProcAddress(library->handle, StringValueCStr(name));
-#else
-    address = dlsym(library->handle, StringValueCStr(name));
-#endif
+    address = dl_sym(library->handle, StringValueCStr(name));
     return address != NULL ? rb_FFI_Pointer_new(address) : Qnil;
 }
 
 static VALUE
 library_dlerror(VALUE self)
 {
-#ifdef WIN32
     char errmsg[1024];
-    FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(),
-            0, errmsg, sizeof(errmsg), NULL);
+    dl_error(errmsg, sizeof(errmsg));
     return rb_tainted_str_new2(errmsg);
-#else
-    return rb_tainted_str_new2(dlerror());
-#endif
 }
 
 static void
@@ -91,17 +82,28 @@ library_free(Library* library)
         // dlclose() on MacOS tends to segfault - avoid it
 #ifndef __APPLE__
         if (library->handle != NULL) {
-# ifdef _WIN32
-            FreeLibrary(library->handle);
-# else
-            dlclose(library->handle);
-# endif
+            dl_close(library->handle);
         }
 #endif
         library->handle = NULL;
         xfree(library);
     }
 }
+
+#ifdef _WIN32
+static void*
+dl_open(const char* name, int flags)
+{
+    return LoadLibraryEx(name, NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
+}
+
+static void
+dl_error(char* buf, int size)
+{
+    FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(),
+            0, buf, size, NULL);
+}
+#endif
 
 void
 rb_FFI_NativeLibrary_Init()
@@ -113,12 +115,12 @@ rb_FFI_NativeLibrary_Init()
     rb_define_method(classLibrary, "find_symbol", library_dlsym, 1);
     rb_define_method(classLibrary, "last_error", library_dlerror, 0);
     rb_define_attr(classLibrary, "name", 1, 0);
-#ifndef _WIN32
+
 #define DEF(x) rb_define_const(classLibrary, "RTLD_" #x, UINT2NUM(RTLD_##x))
     DEF(LAZY);
     DEF(NOW);
     DEF(GLOBAL);
     DEF(LOCAL);
-#endif // _WIN32
+
 }
 
