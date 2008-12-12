4012dcf31827265f5db707bf70362ff147c6924c
Wayne Meissner <wmeissner@gmail.com>
2008-12-12 15:57:20 +1000

    Make libffi build on cygwin

--- ext/ffi_c/libffi.mk
+++ ext/ffi_c/libffi.mk
@@ -4,7 +4,8 @@ $(LIBFFI):
 	@mkdir -p $(LIBFFI_BUILD_DIR)
 	@if [ ! -f $(LIBFFI_BUILD_DIR)/Makefile ]; then \
 	    echo "Configuring libffi"; \
-	    cd $(LIBFFI_BUILD_DIR) && env CC="$(CC)" LD="$(LD)" CFLAGS="$(FFI_CFLAGS)" \
-		$(FFI_CONFIGURE) $(LIBFFI_HOST) > /dev/null; \
+	    cd $(LIBFFI_BUILD_DIR) && \
+		/usr/bin/env CC="$(CC)" LD="$(LD)" CFLAGS="$(FFI_CFLAGS)" \
+		/bin/sh $(FFI_CONFIGURE) $(LIBFFI_HOST) > /dev/null; \
 	fi
 	cd $(LIBFFI_BUILD_DIR) && $(MAKE)
