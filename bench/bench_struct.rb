5cf40581f51c585d97575d1fcbd34817b3c7cd8b
Wayne Meissner <wmeissner@gmail.com>
2009-01-09 20:44:09 +1000

    Add benchmarks for functions which take a pointer/buffer for the various in/out/inout combinations

--- bench/bench_struct.rb
+++ bench/bench_struct.rb
@@ -9,10 +9,31 @@ module StructBench
   extend FFI::Library
   ffi_lib LIBTEST_PATH
   attach_function :bench_s32_v, [ :int ], :void
+  attach_function :bench_struct_in, :ptr_ret_int32_t, [ :buffer_in, :int ], :void
+  attach_function :bench_struct_out, :ptr_ret_int32_t, [ :buffer_out, :int ], :void
+  attach_function :bench_struct_inout, :ptr_ret_int32_t, [ :buffer_inout, :int ], :void
 end
 class TestStruct < FFI::Struct
   layout :i, :int, :p, :pointer
 end
+puts "Benchmark FFI call(Struct.alloc_in) performance, #{iter}x"
+10.times {
+  puts Benchmark.measure {
+    iter.times { StructBench.bench_struct_in(TestStruct.alloc_in, 0) }
+  }
+}
+puts "Benchmark FFI call(Struct.alloc_out) performance, #{iter}x"
+10.times {
+  puts Benchmark.measure {
+    iter.times { StructBench.bench_struct_out(TestStruct.alloc_out, 0) }
+  }
+}
+puts "Benchmark FFI call(Struct.alloc_inout) performance, #{iter}x"
+10.times {
+  puts Benchmark.measure {
+    iter.times { StructBench.bench_struct_inout(TestStruct.alloc_inout, 0) }
+  }
+}
 s = TestStruct.new
 puts "Benchmark FFI Struct.get(:int) performance, #{iter}x"
 10.times {
