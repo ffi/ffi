b3c336b917d18faa8f2f29a22cfbc1bd93163164
Wayne Meissner <wmeissner@gmail.com>
2009-01-15 10:01:53 +1000

    Add additional benchmarks with a pre-allocated native Timeval struct, and an on-the-fly allocated Timeval struct

--- bench/bench_gettimeofday.rb
+++ bench/bench_gettimeofday.rb
@@ -3,7 +3,7 @@ require 'ffi'
 
 module Posix
   extend FFI::Library
-  attach_function :gettimeofday, [ :pointer, :pointer ], :int
+  attach_function :gettimeofday, [ :buffer_out, :pointer ], :int
 end
 class Timeval < FFI::Struct
   layout :tv_sec, :ulong, :tv_nsec, :ulong
@@ -18,10 +18,22 @@ puts "Benchmark FFI gettimeofday(2) (nil, nil) performance, #{iter}x"
   }
 }
 puts "Benchmark FFI gettimeofday(2) (Timeval.alloc_out, nil) performance, #{iter}x"
-
 10.times {
   puts Benchmark.measure {
     iter.times { Posix.gettimeofday(Timeval.alloc_out, nil) }
+  }
+}
+puts "Benchmark FFI gettimeofday(2) (Timeval.new(MemoryPointer.new), nil) performance, #{iter}x"
+10.times {
+  puts Benchmark.measure {
+    iter.times { Posix.gettimeofday(Timeval.new(MemoryPointer.new(Timeval)), nil) }
+  }
+}
+puts "Benchmark FFI gettimeofday(2) (pre allocated pointer, nil) performance, #{iter}x"
+10.times {
+  t = Timeval.new MemoryPointer.new(Timeval)
+  puts Benchmark.measure {
+    iter.times { Posix.gettimeofday(t, nil) }
   }
 }
 puts "Benchmark Time.now performance, #{iter}x"
