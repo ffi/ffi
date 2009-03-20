901da744737cdedacfc36696e40d51944275c04e
Wayne Meissner <wmeissner@gmail.com>
2009-03-20 12:02:49 +1000

    Cleanup, only use buffer_mark when chaining buffers via '+', and only use buffer_release when allocating new Buffers

--- ext/ffi_c/Buffer.c
+++ ext/ffi_c/Buffer.c
@@ -25,7 +25,23 @@ static VALUE
 buffer_allocate(VALUE klass)
 {
     Buffer* buffer;
-    return Data_Make_Struct(klass, Buffer, buffer_mark, buffer_release, buffer);
+    VALUE obj;
+
+    obj = Data_Make_Struct(klass, Buffer, NULL, buffer_release, buffer);
+    buffer->parent = Qnil;
+
+    return obj;
+}
+
+static void
+buffer_release(Buffer* ptr)
+{
+    if (ptr->storage != NULL) {
+        free(ptr->storage);
+        ptr->storage = NULL;
+    }
+    
+    xfree(ptr);
 }
 
 static VALUE
@@ -34,27 +50,30 @@ buffer_initialize(int argc, VALUE* argv, VALUE self)
     VALUE size = Qnil, count = Qnil, clear = Qnil;
     Buffer* p;
     unsigned long msize;
-    void* memory;
     int nargs;
-
+    
     nargs = rb_scan_args(argc, argv, "12", &size, &count, &clear);
     msize = rb_FFI_type_size(size) * (nargs > 1 ? NUM2LONG(count) : 1);
-    memory = malloc(msize + 7);
-    if (memory == NULL) {
+
+    Data_Get_Struct(self, Buffer, p);
+    p->memory.size = msize;
+
+    p->storage = malloc(msize + 7);
+    if (p->storage == NULL) {
         rb_raise(rb_eNoMemError, "Failed to allocate memory size=%lu bytes", msize);
     }
-    Data_Get_Struct(self, Buffer, p);
-    p->storage = memory;
-    p->memory.size = msize;
+
     /* ensure the memory is aligned on at least a 8 byte boundary */
-    p->memory.address = (void *) (((uintptr_t) memory + 0x7) & (uintptr_t) ~0x7UL);
-    p->parent = Qnil;
+    p->memory.address = (void *) (((uintptr_t) p->storage + 0x7) & (uintptr_t) ~0x7UL);
+    
     if (nargs > 2 && RTEST(clear) && p->memory.size > 0) {
         memset(p->memory.address, 0, p->memory.size);
     }
+
     if (rb_block_given_p()) {
-        return rb_rescue(rb_yield, self, buffer_free, self);
+        return rb_ensure(rb_yield, self, buffer_free, self);
     }
+
     return self;
 }
 
@@ -73,10 +92,12 @@ buffer_plus(VALUE self, VALUE offset)
     long off = NUM2LONG(offset);
 
     checkBounds(&ptr->memory, off, 1);
-    retval = Data_Make_Struct(classBuffer, Buffer, buffer_mark, buffer_release, p);
-    p->memory.address = ptr->memory.address + off;;
+
+    retval = Data_Make_Struct(classBuffer, Buffer, buffer_mark, -1, p);
+    p->memory.address = ptr->memory.address + off;
     p->memory.size = ptr->memory.size - off;
     p->parent = self;
+
     return retval;
 }
 
@@ -84,7 +105,9 @@ static VALUE
 buffer_inspect(VALUE self)
 {
     char tmp[100];
+
     snprintf(tmp, sizeof(tmp), "#<Buffer size=%ld>", BUFFER(self)->memory.size);
+    
     return rb_str_new2(tmp);
 }
 
@@ -92,30 +115,21 @@ buffer_inspect(VALUE self)
 static VALUE
 buffer_free(VALUE self)
 {
-    Buffer* ptr = BUFFER(self);
-    if (ptr->parent == Qnil && ptr->storage != NULL) {
-        free(ptr->storage);
-        ptr->storage = NULL;
-    }
-    return self;
-}
+    Buffer* ptr;
 
-static void
-buffer_release(Buffer* ptr)
-{
-    if (ptr->parent == Qnil && ptr->storage != NULL) {
+    Data_Get_Struct(self, Buffer, ptr);
+    if (ptr->storage != NULL) {
         free(ptr->storage);
         ptr->storage = NULL;
     }
-    xfree(ptr);
+
+    return self;
 }
 
 static void
 buffer_mark(Buffer* ptr)
 {
-    if (ptr->parent != Qnil) {
-        rb_gc_mark(ptr->parent);
-    }
+    rb_gc_mark(ptr->parent);
 }
 
 void
@@ -123,6 +137,8 @@ rb_FFI_Buffer_Init()
 {
     VALUE moduleFFI = rb_define_module("FFI");
     classBuffer = rb_define_class_under(moduleFFI, "Buffer", rb_FFI_AbstractMemory_class);
+
+    rb_global_variable(&classBuffer);
     rb_define_alloc_func(classBuffer, buffer_allocate);
 
     rb_define_singleton_method(classBuffer, "alloc_inout", buffer_alloc_inout, -1);
@@ -133,4 +149,3 @@ rb_FFI_Buffer_Init()
     rb_define_method(classBuffer, "inspect", buffer_inspect, 0);
     rb_define_method(classBuffer, "+", buffer_plus, 1);
 }
-
