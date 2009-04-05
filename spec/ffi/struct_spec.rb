ad1b2f01df68faec98ac3494e0e69f66075672bb
Andrea Fazzi <andrea.fazzi@alcacoop.it>
2009-04-05 14:27:50 +0200

    Pull up 'include FFI' in struct_spec.rb. This fix allows to run the specs issuing:
    spec spec/ffi/struct_spec.rb

--- spec/ffi/struct_spec.rb
+++ spec/ffi/struct_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "spec_helper"))
+include FFI
 describe "Struct tests" do
-  include FFI
   StructTypes = {
     's8' => :char,
     's16' => :short,
