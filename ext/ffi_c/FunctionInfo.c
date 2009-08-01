a6e0ef9e2d85e42e8b43cfe8a9f7985a53498aee
Wayne Meissner <wmeissner@gmail.com>
2009-08-01 17:41:04 +1000

    Remove unused includes and cleanup fninfo_free()

--- ext/ffi_c/FunctionInfo.c
+++ ext/ffi_c/FunctionInfo.c
@@ -1,16 +1,37 @@
+/*
+ * Copyright (c) 2009, Wayne Meissner
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * The name of the author or authors may not be used to endorse or promote
+ *   products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <sys/param.h>
 #include <sys/types.h>
-#ifndef _WIN32
-#  include <sys/mman.h>
-#endif
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <errno.h>
 #include <ruby.h>
-#if defined(HAVE_NATIVETHREAD) && !defined(_WIN32) && !defined(__WIN32__)
-# include <pthread.h>
-#endif
 
 #include <ffi.h>
 #include "rbffi.h"
@@ -58,22 +79,10 @@ fninfo_mark(FunctionInfo* fnInfo)
 static void
 fninfo_free(FunctionInfo* fnInfo)
 {
-    if (fnInfo->parameterTypes != NULL) {
-        xfree(fnInfo->parameterTypes);
-    }
-
-    if (fnInfo->ffiParameterTypes != NULL) {
-        xfree(fnInfo->ffiParameterTypes);
-    }
-
-    if (fnInfo->nativeParameterTypes != NULL) {
-        xfree(fnInfo->nativeParameterTypes);
-    }
-
-    if (fnInfo->callbackParameters != NULL) {
-        xfree(fnInfo->callbackParameters);
-    }
-
+    xfree(fnInfo->parameterTypes);
+    xfree(fnInfo->ffiParameterTypes);
+    xfree(fnInfo->nativeParameterTypes);
+    xfree(fnInfo->callbackParameters);
     xfree(fnInfo);
 }
 
