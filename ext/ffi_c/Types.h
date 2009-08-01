27afb27814c5fc635b3f6b106c141a093e179c1a
Wayne Meissner <wmeissner@gmail.com>
2009-08-01 18:03:10 +1000

    Add NATIVE_STRUCT to NativeType

--- ext/ffi_c/Types.h
+++ ext/ffi_c/Types.h
@@ -1,12 +1,39 @@
-#ifndef _TYPES_H
-#define	_TYPES_H
+/*
+ * Copyright (c) 2008, 2009, Wayne Meissner
+ * Copyright (c) 2009, Luc Heinrich <luc@honk-honk.com>
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * The name of the author or authors may not be used to endorse or promote
+ *   products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef RBFFI_TYPES_H
+#define	RBFFI_TYPES_H
 
 #ifdef	__cplusplus
 extern "C" {
 #endif
-#ifdef VOID
-#  undef VOID
-#endif
+
 typedef enum {
     NATIVE_VOID,
     NATIVE_INT8,
@@ -38,6 +65,8 @@ typedef enum {
     NATIVE_VARARGS,
     /** A typedef-ed enum */
     NATIVE_ENUM,
+    /** Struct-by-value param or result */
+    NATIVE_STRUCT,
 } NativeType;
 
 #include <ffi.h>
@@ -50,5 +79,5 @@ void rbffi_Types_Init(VALUE moduleFFI);
 }
 #endif
 
-#endif	/* _TYPES_H */
+#endif	/* RBFFI_TYPES_H */
 
