0faa454dffe67caacdd1b9ca39e3d60835d8e288
Wayne Meissner <wmeissner@gmail.com>
2009-08-03 18:28:50 +1000

    Cleanups

--- ext/ffi_c/Call.c
+++ ext/ffi_c/Call.c
@@ -30,17 +30,11 @@
 
 #include <sys/param.h>
 #include <sys/types.h>
-#ifndef _WIN32
-#  include <sys/mman.h>
-#endif
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <errno.h>
 #include <ruby.h>
-#if defined(HAVE_NATIVETHREAD) && !defined(_WIN32) && !defined(__WIN32__)
-# include <pthread.h>
-#endif
 
 #include <ffi.h>
 #include "rbffi.h"
@@ -49,10 +43,8 @@
 #include "AbstractMemory.h"
 #include "Pointer.h"
 #include "Struct.h"
-#include "Platform.h"
 #include "Function.h"
 #include "Callback.h"
-#include "Types.h"
 #include "Type.h"
 #include "LastError.h"
 #include "Call.h"
@@ -62,13 +54,6 @@
 #    error "RAW argument packing only supported on i386"
 #  endif
 
-#define INT8_SIZE (sizeof(char))
-#define INT16_SIZE (sizeof(short))
-#define INT32_SIZE (sizeof(int))
-#define INT64_SIZE (sizeof(long long))
-#define FLOAT32_SIZE (sizeof(float))
-#define FLOAT64_SIZE (sizeof(double))
-#define ADDRESS_SIZE (sizeof(void *))
 #define INT8_ADJ (4)
 #define INT16_ADJ (4)
 #define INT32_ADJ (4)
@@ -89,6 +74,8 @@ static void* callback_param(VALUE proc, VALUE cbinfo);
 static inline int getSignedInt(VALUE value, int type, int minValue, int maxValue, const char* typeName, VALUE enums);
 static inline int getUnsignedInt(VALUE value, int type, int maxValue, const char* typeName);
 static inline unsigned int getUnsignedInt32(VALUE value, int type);
+static inline void* getPointer(VALUE value, int type);
+static inline char* getString(VALUE value, int type);
 
 static ID id_to_ptr, id_map_symbol;
 
@@ -108,24 +95,34 @@ rbffi_SetupCallParams(int argc, VALUE* argv, int paramCount, NativeType* paramTy
             rb_raise(rb_eArgError, "wrong number of arguments (%d for %d)", argc, paramCount);
         }
     }
+
     argCount = paramCount != -1 ? paramCount : argc;
+
     for (i = 0, argidx = 0, cbidx = 0; i < argCount; ++i) {
         int type = argidx < argc ? TYPE(argv[argidx]) : T_NONE;
         ffiValues[i] = param;
+
         switch (paramTypes[i]) {
+
             case NATIVE_INT8:
                 param->s8 = getSignedInt(argv[argidx++], type, -128, 127, "char", Qnil);
                 ADJ(param, INT8);
                 break;
+
+
             case NATIVE_INT16:
                 param->s16 = getSignedInt(argv[argidx++], type, -0x8000, 0x7fff, "short", Qnil);
                 ADJ(param, INT16);
                 break;
+
+
             case NATIVE_INT32:
             case NATIVE_ENUM:
                 param->s32 = getSignedInt(argv[argidx++], type, -0x80000000, 0x7fffffff, "int", enums);
                 ADJ(param, INT32);
                 break;
+
+
             case NATIVE_BOOL:
                 if (type != T_TRUE && type != T_FALSE) {
                     rb_raise(rb_eTypeError, "Expected a Boolean parameter");
@@ -133,19 +130,27 @@ rbffi_SetupCallParams(int argc, VALUE* argv, int paramCount, NativeType* paramTy
                 param->s32 = argv[argidx++] == Qtrue;
                 ADJ(param, INT32);
                 break;
+
+
             case NATIVE_UINT8:
                 param->u8 = getUnsignedInt(argv[argidx++], type, 0xff, "unsigned char");
                 ADJ(param, INT8);
                 break;
+
+
             case NATIVE_UINT16:
                 param->u16 = getUnsignedInt(argv[argidx++], type, 0xffff, "unsigned short");
                 ADJ(param, INT16);
                 break;
+
+
             case NATIVE_UINT32:
                 /* Special handling/checking for unsigned 32 bit integers */
                 param->u32 = getUnsignedInt32(argv[argidx++], type);
                 ADJ(param, INT32);
                 break;
+
+
             case NATIVE_INT64:
                 if (type != T_FIXNUM && type != T_BIGNUM) {
                     rb_raise(rb_eTypeError, "Expected an Integer parameter");
@@ -154,6 +159,8 @@ rbffi_SetupCallParams(int argc, VALUE* argv, int paramCount, NativeType* paramTy
                 ADJ(param, INT64);
                 ++argidx;
                 break;
+
+
             case NATIVE_UINT64:
                 if (type != T_FIXNUM && type != T_BIGNUM) {
                     rb_raise(rb_eTypeError, "Expected an Integer parameter");
@@ -162,6 +169,8 @@ rbffi_SetupCallParams(int argc, VALUE* argv, int paramCount, NativeType* paramTy
                 ADJ(param, INT64);
                 ++argidx;
                 break;
+
+
             case NATIVE_FLOAT32:
                 if (type != T_FLOAT && type != T_FIXNUM) {
                     rb_raise(rb_eTypeError, "Expected a Float parameter");
@@ -170,6 +179,7 @@ rbffi_SetupCallParams(int argc, VALUE* argv, int paramCount, NativeType* paramTy
                 ADJ(param, FLOAT32);
                 ++argidx;
                 break;
+
             case NATIVE_FLOAT64:
                 if (type != T_FLOAT && type != T_FIXNUM) {
                     rb_raise(rb_eTypeError, "Expected a Float parameter");
@@ -178,50 +188,22 @@ rbffi_SetupCallParams(int argc, VALUE* argv, int paramCount, NativeType* paramTy
                 ADJ(param, FLOAT64);
                 ++argidx;
                 break;
+
+
             case NATIVE_STRING:
-                if (type == T_STRING) {
-                    if (rb_safe_level() >= 1 && OBJ_TAINTED(argv[argidx])) {
-                        rb_raise(rb_eSecurityError, "Unsafe string parameter");
-                    }
-                    param->ptr = StringValueCStr(argv[argidx]);
-                } else if (type == T_NIL) {
-                    param->ptr = NULL;
-                } else {
-                    rb_raise(rb_eArgError, "Invalid String value");
-                }
+                param->ptr = getString(argv[argidx++], type);
                 ADJ(param, ADDRESS);
-                ++argidx;
                 break;
+
             case NATIVE_POINTER:
             case NATIVE_BUFFER_IN:
             case NATIVE_BUFFER_OUT:
             case NATIVE_BUFFER_INOUT:
-                if (type == T_DATA && rb_obj_is_kind_of(argv[argidx], rbffi_AbstractMemoryClass)) {
-                    param->ptr = ((AbstractMemory *) DATA_PTR(argv[argidx]))->address;
-                } else if (type == T_DATA && rb_obj_is_kind_of(argv[argidx], rbffi_StructClass)) {
-                    AbstractMemory* memory = ((Struct *) DATA_PTR(argv[argidx]))->pointer;
-                    param->ptr = memory != NULL ? memory->address : NULL;
-                } else if (type == T_STRING) {
-                    if (rb_safe_level() >= 1 && OBJ_TAINTED(argv[argidx])) {
-                        rb_raise(rb_eSecurityError, "Unsafe string parameter");
-                    }
-                    param->ptr = StringValuePtr(argv[argidx]);
-                } else if (type == T_NIL) {
-                    param->ptr = NULL;
-                } else if (rb_respond_to(argv[argidx], id_to_ptr)) {
-                    VALUE ptr = rb_funcall2(argv[argidx], id_to_ptr, 0, NULL);
-                    if (rb_obj_is_kind_of(ptr, rbffi_AbstractMemoryClass) && TYPE(ptr) == T_DATA) {
-                        param->ptr = ((AbstractMemory *) DATA_PTR(ptr))->address;
-                    } else {
-                        rb_raise(rb_eArgError, "to_ptr returned an invalid pointer");
-                    }
-
-                } else {
-                    rb_raise(rb_eArgError, ":pointer argument is not a valid pointer");
-                }
+                param->ptr = getPointer(argv[argidx++], type);
                 ADJ(param, ADDRESS);
-                ++argidx;
                 break;
+
+
             case NATIVE_FUNCTION:
             case NATIVE_CALLBACK:
                 if (callbackProc != Qnil) {
@@ -232,11 +214,14 @@ rbffi_SetupCallParams(int argc, VALUE* argv, int paramCount, NativeType* paramTy
                 }
                 ADJ(param, ADDRESS);
                 break;
+
+
             default:
                 rb_raise(rb_eArgError, "Invalid parameter type: %d", paramTypes[i]);
         }
     }
 }
+
 
 VALUE
 rbffi_CallFunction(int argc, VALUE* argv, void* function, FunctionInfo* fnInfo)
@@ -270,19 +255,22 @@ static inline int
 getSignedInt(VALUE value, int type, int minValue, int maxValue, const char* typeName, VALUE enums)
 {
     int i;
+
     if (type == T_SYMBOL && enums != Qnil) {
         value = rb_funcall2(enums, id_map_symbol, 1, &value);
         if (value == Qnil) {
             rb_raise(rb_eTypeError, "Expected a valid enum constant");
         }
-    }
-    else if (type != T_FIXNUM && type != T_BIGNUM) {
+    
+    } else if (type != T_FIXNUM && type != T_BIGNUM) {
         rb_raise(rb_eTypeError, "Expected an Integer parameter");
     }
+
     i = NUM2INT(value);
     if (i < minValue || i > maxValue) {
         rb_raise(rb_eRangeError, "Value %d outside %s range", i, typeName);
     }
+
     return i;
 }
 
@@ -290,13 +278,16 @@ static inline int
 getUnsignedInt(VALUE value, int type, int maxValue, const char* typeName)
 {
     int i;
+
     if (type != T_FIXNUM && type != T_BIGNUM) {
         rb_raise(rb_eTypeError, "Expected an Integer parameter");
     }
+
     i = NUM2INT(value);
     if (i < 0 || i > maxValue) {
         rb_raise(rb_eRangeError, "Value %d outside %s range", i, typeName);
     }
+
     return i;
 }
 
@@ -305,16 +296,72 @@ static inline unsigned int
 getUnsignedInt32(VALUE value, int type)
 {
     long long i;
+
     if (type != T_FIXNUM && type != T_BIGNUM) {
         rb_raise(rb_eTypeError, "Expected an Integer parameter");
     }
+
     i = NUM2LL(value);
     if (i < 0L || i > 0xffffffffL) {
         rb_raise(rb_eRangeError, "Value %lld outside unsigned int range", i);
     }
+
     return (unsigned int) i;
 }
 
+static inline void*
+getPointer(VALUE value, int type)
+{
+    if (type == T_DATA && rb_obj_is_kind_of(value, rbffi_AbstractMemoryClass)) {
+
+        return ((AbstractMemory *) DATA_PTR(value))->address;
+
+    } else if (type == T_DATA && rb_obj_is_kind_of(value, rbffi_StructClass)) {
+
+        AbstractMemory* memory = ((Struct *) DATA_PTR(value))->pointer;
+        return memory != NULL ? memory->address : NULL;
+
+    } else if (type == T_STRING) {
+
+        if (rb_safe_level() >= 1 && OBJ_TAINTED(value)) {
+            rb_raise(rb_eSecurityError, "Unsafe string parameter");
+        }
+        return StringValuePtr(value);
+
+    } else if (type == T_NIL) {
+
+        return NULL;
+
+    } else if (rb_respond_to(value, id_to_ptr)) {
+
+        VALUE ptr = rb_funcall2(value, id_to_ptr, 0, NULL);
+        if (rb_obj_is_kind_of(ptr, rbffi_AbstractMemoryClass) && TYPE(ptr) == T_DATA) {
+            return ((AbstractMemory *) DATA_PTR(ptr))->address;
+        }
+        rb_raise(rb_eArgError, "to_ptr returned an invalid pointer");
+    }
+
+    rb_raise(rb_eArgError, ":pointer argument is not a valid pointer");
+    return NULL;
+}
+
+static inline char*
+getString(VALUE value, int type)
+{
+    if (type == T_STRING) {
+
+        if (rb_safe_level() >= 1 && OBJ_TAINTED(value)) {
+            rb_raise(rb_eSecurityError, "Unsafe string parameter");
+        }
+
+        return StringValueCStr(value);
+
+    } else if (type == T_NIL) {
+        return NULL;
+    }
+
+    rb_raise(rb_eArgError, "Invalid String value");
+}
 
 static void*
 callback_param(VALUE proc, VALUE cbInfo)
@@ -332,6 +379,7 @@ callback_param(VALUE proc, VALUE cbInfo)
     }
 
     callback = rbffi_NativeCallback_ForProc(proc, cbInfo);
+
     return ((NativeCallback *) DATA_PTR(callback))->code;
 }
 
