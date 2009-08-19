c70259c686f7d2caa9fc55ef35921b0f0784b4a9
Andrea Fazzi <andrea.fazzi@alcacoop.it>
2009-08-19 19:03:43 +0200

    Add preliminary specs for FFI::Function

---
+++ spec/ffi/function_spec.rb
@@ -0,0 +1,43 @@
+require File.expand_path(File.join(File.dirname(__FILE__), "spec_helper"))
+
+describe FFI::Function do
+  module LibTest
+    extend FFI::Library
+    ffi_lib TestLibrary::PATH
+    attach_function :testFunctionAdd, [:int, :int, :pointer], :int
+    attach_function :testAddPointer, [], :pointer
+  end
+  it 'is initialized with a signature and a block' do
+    FFI::Function.new(:int, []) { }
+  end
+  it 'raises an error when passing a wrong signature' do
+    lambda { FFI::Function.new([], :int).new { } }.should raise_error TypeError 
+  end
+  it 'returns a native pointer' do
+    FFI::Function.new(:int, []) { }.kind_of? FFI::Pointer
+  end
+  it 'can be used as callback from C passing to it a block' do
+    function_add = FFI::Function.new(:int, [:int, :int]) { |a, b| a + b }
+    LibTest.testFunctionAdd(10, 10, function_add).should == 20
+  end
+  it 'can be used as callback from C passing to it a Proc object' do
+    function_add = FFI::Function.new(:int, [:int, :int], Proc.new { |a, b| a + b })
+    LibTest.testFunctionAdd(10, 10, function_add).should == 20
+  end
+  it 'can be used to wrap an existing function pointer' do
+    FFI::Function.new(:int, [:int, :int], LibTest.testAddPointer).call(10, 10).should == 20
+  end
+  it 'can be attached to a module' do
+    module Foo; end
+    fp = FFI::Function.new(:int, [:int, :int], LibTest.testAddPointer)
+    fp.attach(Foo, 'add')
+    Foo.add(10, 10).should == 20
+  end
+  it 'can be used to extend an object' do
+    foo = Object.new
+    class << foo
+      FFI::Function.new(:int, [:int, :int], LibTest.testAddPointer).attach(self, 'add')
+    end
+    foo.add(10, 10).should == 20    
+  end
+end
