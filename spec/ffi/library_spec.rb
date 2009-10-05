919d099618685cc0304d383cb8f7aa97f179a5a0
Andrea Fazzi <andrea.fazzi@alcacoop.it>
2009-10-05 13:16:28 +0200

    Exclude library_spec.rb examples that are specific for *nix system when running specs on windows

--- spec/ffi/library_spec.rb
+++ spec/ffi/library_spec.rb
@@ -1,57 +1,61 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "spec_helper"))
 describe "Library" do
-  it "attach_function with no library specified" do
-    lambda {
-      Module.new do |m|
-        m.extend FFI::Library
-        attach_function :getpid, [ ], :uint
-      end
-    }.should_not raise_error
+
+  unless Config::CONFIG['target_os'] =~ /mingw/ || Config::CONFIG['target_os'] =~ /win/
+    it "attach_function with no library specified" do
+      lambda {
+        Module.new do |m|
+          m.extend FFI::Library
+          attach_function :getpid, [ ], :uint
+        end
+      }.should_not raise_error
+    end
+    it "attach_function :getpid from this process" do
+      lambda {
+        Module.new do |m|
+          m.extend FFI::Library
+          attach_function :getpid, [ ], :uint
+        end.getpid.should == Process.pid
+      }.should_not raise_error
+    end
+    it "attach_function :getpid from [ 'c', 'libc.so.6'] " do
+      lambda {
+        Module.new do |m|
+          m.extend FFI::Library
+          ffi_lib 'c', 'libc.so.6'
+          attach_function :getpid, [ ], :uint
+        end.getpid.should == Process.pid
+      }.should_not raise_error
+    end
+    it "attach_function :getpid from [ 'libc.so.6', 'c' ] " do
+      lambda {
+        Module.new do |m|
+          m.extend FFI::Library
+          ffi_lib 'libc.so.6', 'c'
+          attach_function :getpid, [ ], :uint
+        end.getpid.should == Process.pid
+      }.should_not raise_error
+    end
+    it "attach_function :getpid from [ 'libfubar.so.0xdeadbeef', nil, 'c' ] " do
+      lambda {
+        Module.new do |m|
+          m.extend FFI::Library
+          ffi_lib 'libfubar.so.0xdeadbeef', nil, 'c'
+          attach_function :getpid, [ ], :uint
+        end.getpid.should == Process.pid
+      }.should_not raise_error
+    end
+    it "attach_function :getpid from [ 'libfubar.so.0xdeadbeef' ] " do
+      lambda {
+        Module.new do |m|
+          m.extend FFI::Library
+          ffi_lib 'libfubar.so.0xdeadbeef'
+          attach_function :getpid, [ ], :uint
+        end.getpid.should == Process.pid
+      }.should raise_error(LoadError)
+    end
   end
-  it "attach_function :getpid from this process" do
-    lambda {
-      Module.new do |m|
-        m.extend FFI::Library
-        attach_function :getpid, [ ], :uint
-      end.getpid.should == Process.pid
-    }.should_not raise_error
-  end
-  it "attach_function :getpid from [ 'c', 'libc.so.6'] " do
-    lambda {
-      Module.new do |m|
-        m.extend FFI::Library
-        ffi_lib 'c', 'libc.so.6'
-        attach_function :getpid, [ ], :uint
-      end.getpid.should == Process.pid
-    }.should_not raise_error
-  end
-  it "attach_function :getpid from [ 'libc.so.6', 'c' ] " do
-    lambda {
-      Module.new do |m|
-        m.extend FFI::Library
-        ffi_lib 'libc.so.6', 'c'
-        attach_function :getpid, [ ], :uint
-      end.getpid.should == Process.pid
-    }.should_not raise_error
-  end
-  it "attach_function :getpid from [ 'libfubar.so.0xdeadbeef', nil, 'c' ] " do
-    lambda {
-      Module.new do |m|
-        m.extend FFI::Library
-        ffi_lib 'libfubar.so.0xdeadbeef', nil, 'c'
-        attach_function :getpid, [ ], :uint
-      end.getpid.should == Process.pid
-    }.should_not raise_error
-  end
-  it "attach_function :getpid from [ 'libfubar.so.0xdeadbeef' ] " do
-    lambda {
-      Module.new do |m|
-        m.extend FFI::Library
-        ffi_lib 'libfubar.so.0xdeadbeef'
-        attach_function :getpid, [ ], :uint
-      end.getpid.should == Process.pid
-    }.should raise_error(LoadError)
-  end
+
   def gvar_lib(name, type)
     Module.new do |m|
       m.extend FFI::Library
