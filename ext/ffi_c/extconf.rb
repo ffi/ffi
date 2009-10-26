8d946b866b290655e52475f07fcdbf2cf79840cf
Wayne Meissner <wmeissner@gmail.com>
2009-10-26 17:45:41 +1000

    Allow the system libffi to be used where it is at least minimally functional

--- ext/ffi_c/extconf.rb
+++ ext/ffi_c/extconf.rb
@@ -4,21 +4,23 @@ require 'rbconfig'
 dir_config("ffi_c")
 
 IS_MAC = Config::CONFIG['host_os'] =~ /^darwin/
-have_closure_alloc = have_library("ffi", "ffi_closure_alloc", [ "ffi.h" ])
-$defs.push("-DHAVE_FFI_CLOSURE_ALLOC") if have_closure_alloc
-libffi_ok = have_closure_alloc && !IS_MAC
+pkg_config("libffi")
+#have_libffi = find_library("ffi", "ffi_call", `pkg-config libffi --cflags --libs`)
+have_ffi_call = have_library("ffi", "ffi_call", [ "ffi.h" ])
+have_prep_closure = have_library("ffi", "ffi_prep_closure", [ "ffi.h" ])
+libffi_ok = have_ffi_call && have_prep_closure
 $defs << "-DHAVE_LIBFFI" if libffi_ok
-$defs << "-DHAVE_EXTCONF_H" if $defs.empty? # needed so create_header works
+$defs << "-DHAVE_RAW_API" if have_library("ffi", "ffi_raw_call", [ "ffi.h" ]) && have_library("ffi", "ffi_prep_raw_closure", [ "ffi.h"])
+
 
 have_func('rb_thread_blocking_region')
-
+$defs << "-DHAVE_EXTCONF_H" if $defs.empty? # needed so create_header works
 create_makefile("ffi_c")
-create_header("extconf.h")
+create_header
 File.open("Makefile", "a") do |mf|
   mf.puts "CPPFLAGS += -Werror -Wunused -Wformat -Wimplicit -Wreturn-type"
   unless libffi_ok 
     mf.puts "LIBFFI_HOST=--host=#{Config::CONFIG['host_alias']}" if Config::CONFIG.has_key?("host_alias")
-    mf.puts "FFI_MMAP_EXEC=-DFFI_MMAP_EXEC_WRIT=#{Config::CONFIG['host_os'] =~ /(win32|mingw)/ ? 0 : 1}"
     if Config::CONFIG['host_os'].downcase =~ /darwin/
       mf.puts "include ${srcdir}/libffi.darwin.mk"
     elsif Config::CONFIG['host_os'].downcase =~ /bsd/
