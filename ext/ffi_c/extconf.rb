b84a2e1ea892949be2684f0266bd187014c475f5
Wayne Meissner <wmeissner@gmail.com>
2009-11-06 05:30:42 +1000

    Clean up CFLAGS changes, and only re-open the Makefile if needed

--- ext/ffi_c/extconf.rb
+++ ext/ffi_c/extconf.rb
@@ -9,15 +9,18 @@ have_prep_closure = have_func("ffi_prep_closure")
 libffi_ok = have_ffi_call && have_prep_closure
 $defs << "-DHAVE_LIBFFI" if libffi_ok
 $defs << "-DHAVE_RAW_API" if have_func("ffi_raw_call") && have_func("ffi_prep_raw_closure")
-
-
 have_func('rb_thread_blocking_region')
+
 $defs << "-DHAVE_EXTCONF_H" if $defs.empty? # needed so create_header works
-create_makefile("ffi_c")
+
 create_header
-File.open("Makefile", "a") do |mf|
-  mf.puts "CPPFLAGS += -Werror -Wunused -Wformat -Wimplicit -Wreturn-type"
-  unless libffi_ok 
+
+$CFLAGS << "-mwin32" if Config::CONFIG['host_os'] =~ /cygwin/
+$CFLAGS << "-Werror -Wunused -Wformat -Wimplicit -Wreturn-type"
+
+create_makefile("ffi_c")
+unless libffi_ok
+  File.open("Makefile", "a") do |mf|
     mf.puts "LIBFFI_HOST=--host=#{Config::CONFIG['host_alias']}" if Config::CONFIG.has_key?("host_alias")
     if Config::CONFIG['host_os'].downcase =~ /darwin/
       mf.puts "include ${srcdir}/libffi.darwin.mk"
