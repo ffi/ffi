edf833674d9d67eeee0914e6c32688347167bec7
Wayne Meissner <wmeissner@gmail.com>
2009-11-15 10:21:54 +1000

    Remove lib/ from load path when running under JRuby

--- spec/ffi/spec_helper.rb
+++ spec/ffi/spec_helper.rb
@@ -2,10 +2,14 @@ require 'rubygems'
 require 'rbconfig'
 require 'spec'
 
-if ENV["MRI_FFI"]
+if RUBY_PLATFORM =~/java/
+  libdir = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "lib"))
+  $:.reject! { |p| p == libdir }
+else
   $:.unshift File.join(File.dirname(__FILE__), "..", "..", "lib"),
     File.join(File.dirname(__FILE__), "..", "..", "build", "#{Config::CONFIG['host_cpu''arch']}", "ffi_c", RUBY_VERSION)
 end
+puts "loadpath=#{$:.join(':')}"
 require "ffi"
 
 module TestLibrary
