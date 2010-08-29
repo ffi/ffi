9f672a426333d39b882e29cd420406bfcfc2c9bf
Wayne Meissner <wmeissner@gmail.com>
2010-08-29 18:17:39 +1000

    Use String#bytesize in Pointer#write_string()

--- lib/ffi/pointer.rb
+++ lib/ffi/pointer.rb
@@ -87,10 +87,11 @@ module FFI
       put_bytes(0, str, 0, len)
     end
     def write_string(str, len=nil)
-      len = str.size unless len
+      len = str.bytesize unless len
       # Write the string data without NUL termination
       put_bytes(0, str, 0, len)
     end
+
     def read_array_of_type(type, reader, length)
       ary = []
       size = FFI.type_size(type)
