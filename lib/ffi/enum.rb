87d4614262b8237fe1db7fef5e4e9e589eec19a1
Wayne Meissner <wmeissner@gmail.com>
2010-11-30 18:41:05 +1000

    Pass thru the integer if no symbol for it can be found in Enum#from_native()

--- lib/ffi/enum.rb
+++ lib/ffi/enum.rb
@@ -105,13 +105,15 @@ module FFI
     def to_native(val, ctx)
       @kv_map[val] || if val.is_a?(Integer)
         val
+      elsif val.respond_to?(:to_int)
+        val.to_int
       else
         raise ArgumentError, "invalid enum value, #{val.inspect}"
       end
     end
 
     def from_native(val, ctx)
-      @vk_map[val]
+      @vk_map[val] || val
     end
 
   end
