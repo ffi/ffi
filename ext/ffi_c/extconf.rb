a56154149fe6566b3ed56bda4b4969094ac5c069
Wayne Meissner <wmeissner@gmail.com>
2010-12-28 09:31:00 +1000

    Add -Wno-declaration-after-statement to CFLAGS

--- ext/ffi_c/extconf.rb
+++ ext/ffi_c/extconf.rb
@@ -27,6 +27,7 @@ create_header
 
 $CFLAGS << " -mwin32 " if RbConfig::CONFIG['host_os'] =~ /cygwin/
 #$CFLAGS << " -Werror -Wunused -Wformat -Wimplicit -Wreturn-type "
+$CFLAGS << " -Wno-declaration-after-statement "
 
 create_makefile("ffi_c")
 unless libffi_ok
