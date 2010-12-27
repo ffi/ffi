5cf48d43e216fbb2017ea5b4ed3b7ed99e52b1c7
Wayne Meissner <wmeissner@gmail.com>
2010-12-27 10:49:58 +1000

    Fixup a few spec problems

--- spec/ffi/string_spec.rb
+++ spec/ffi/string_spec.rb
@@ -1,3 +1,19 @@
+#
+# This file is part of ruby-ffi.
+#
+# This code is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License version 3 only, as
+# published by the Free Software Foundation.
+#
+# This code is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+# version 3 for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with this work.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 require File.expand_path(File.join(File.dirname(__FILE__), "spec_helper"))
 describe "String tests" do
   include FFI
@@ -10,14 +26,14 @@ describe "String tests" do
     attach_function :string_null, [ ], :string
   end
   it "MemoryPointer#get_string returns a tainted string" do
-    mp = MemoryPointer.new 1024
+    mp = FFI::MemoryPointer.new 1024
     mp.put_string(0, "test\0")
     str = mp.get_string(0)
     str.tainted?.should == true
   end
   it "String returned by a method is tainted" do
-    mp = MemoryPointer.new :pointer
-    sp = MemoryPointer.new 1024
+    mp = FFI::MemoryPointer.new :pointer
+    sp = FFI::MemoryPointer.new 1024
     sp.put_string(0, "test")
     mp.put_pointer(0, sp)
     str = StrLibTest.ptr_ret_pointer(mp, 0)
@@ -45,9 +61,9 @@ describe "String tests" do
   end
   it "reads an array of strings until encountering a NULL pointer" do
     strings = ["foo", "bar", "baz", "testing", "ffi"]
-    ptrary = MemoryPointer.new(:pointer, 6)
+    ptrary = FFI::MemoryPointer.new(:pointer, 6)
     ary = strings.inject([]) do |a, str|
-      f = MemoryPointer.new(1024)
+      f = FFI::MemoryPointer.new(1024)
       f.put_string(0, str)
       a << f
     end
@@ -57,9 +73,9 @@ describe "String tests" do
   end
   it "reads an array of strings of the size specified, substituting nil when a pointer is NULL" do
     strings = ["foo", "bar", "baz", "testing", "ffi"]
-    ptrary = MemoryPointer.new(:pointer, 6)
+    ptrary = FFI::MemoryPointer.new(:pointer, 6)
     ary = strings.inject([]) do |a, str|
-      f = MemoryPointer.new(1024)
+      f = FFI::MemoryPointer.new(1024)
       f.put_string(0, str)
       a << f
     end
@@ -69,9 +85,9 @@ describe "String tests" do
   end
   it "reads an array of strings, taking a memory offset parameter" do
     strings = ["foo", "bar", "baz", "testing", "ffi"]
-    ptrary = MemoryPointer.new(:pointer, 5)
+    ptrary = FFI::MemoryPointer.new(:pointer, 5)
     ary = strings.inject([]) do |a, str|
-      f = MemoryPointer.new(1024)
+      f = FFI::MemoryPointer.new(1024)
       f.put_string(0, str)
       a << f
     end
@@ -80,9 +96,9 @@ describe "String tests" do
   end
   it "raises an IndexError when trying to read an array of strings out of bounds" do
     strings = ["foo", "bar", "baz", "testing", "ffi"]
-    ptrary = MemoryPointer.new(:pointer, 5)
+    ptrary = FFI::MemoryPointer.new(:pointer, 5)
     ary = strings.inject([]) do |a, str|
-      f = MemoryPointer.new(1024)
+      f = FFI::MemoryPointer.new(1024)
       f.put_string(0, str)
       a << f
     end
@@ -91,9 +107,9 @@ describe "String tests" do
   end
   it "raises an IndexError when trying to read an array of strings using a negative offset" do
     strings = ["foo", "bar", "baz", "testing", "ffi"]
-    ptrary = MemoryPointer.new(:pointer, 5)
+    ptrary = FFI::MemoryPointer.new(:pointer, 5)
     ary = strings.inject([]) do |a, str|
-      f = MemoryPointer.new(1024)
+      f = FFI::MemoryPointer.new(1024)
       f.put_string(0, str)
       a << f
     end
--- spec/ffi/struct_initialize_spec.rb
+++ spec/ffi/struct_initialize_spec.rb
@@ -1,3 +1,18 @@
+#
+# This file is part of ruby-ffi.
+#
+# This code is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License version 3 only, as
+# published by the Free Software Foundation.
+#
+# This code is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+# version 3 for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# version 3 along with this work.  If not, see <http://www.gnu.org/licenses/>.
+#
 require File.expand_path(File.join(File.dirname(__FILE__), "spec_helper"))
 
 describe FFI::Struct, ' with an initialize function' do
@@ -20,7 +35,7 @@ describe FFI::ManagedStruct, ' with an initialize function' do
       layout :string, :string
       attr_accessor :magic
       def initialize
-        super MemoryPointer.new(:pointer).put_int(0, 0x1234).get_pointer(0)
+        super FFI::MemoryPointer.new(:pointer).put_int(0, 0x1234).get_pointer(0)
         self.magic = 42
       end
       def self.release;end
