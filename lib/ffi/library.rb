0c8f87faa4911ee47982ccaf150b48e404c001de
Kim Burgestrand <kim@burgestrand.se>
2011-05-12 15:35:57 +0200

    Adjust wording for FFI::Library#ffi_lib_flags docs

--- lib/ffi/library.rb
+++ lib/ffi/library.rb
@@ -125,7 +125,7 @@ module FFI
       :now => DynamicLibrary::RTLD_NOW
     }
 
-    # Allows setting library flags for {#ffi_lib}
+    # Sets library flags for {#ffi_lib}
     # 
     # @example
     #   ffi_lib_flags(:lazy, :local) # => 5
