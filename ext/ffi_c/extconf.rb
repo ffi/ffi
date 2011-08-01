164385baeb8e35b30ac7447e85b627d35ae7ef64
Wayne Meissner <wmeissner@gmail.com>
2011-08-01 20:31:29 +1000

    Ruby 1.9.3 hides ruby_thread_gvl_p(), but shows it as available, so just don't test for it, so the fallback path is used.

--- ext/ffi_c/extconf.rb
+++ ext/ffi_c/extconf.rb
@@ -15,7 +15,7 @@ unless RbConfig::CONFIG['host_os'] =~ /mswin32|mingw32/
 end
 
 have_func('rb_thread_blocking_region')
-have_func('ruby_thread_has_gvl_p')
+have_func('ruby_thread_has_gvl_p') unless RUBY_VERSION >= "1.9.3"
 have_func('ruby_native_thread_p')
 have_func('rb_thread_call_with_gvl')
 
