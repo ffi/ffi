dc4b69266dac13b048fb94eb4b6e4a75a7e4731e
Wayne Meissner <wmeissner@gmail.com>
2011-08-06 18:51:00 +1000

    Fix issue #127 - ffi gem should not load on non-MRI rubies

--- lib/ffi.rb
+++ lib/ffi.rb
@@ -1,11 +1,13 @@
-begin
-  if RUBY_VERSION =~ /1.8/
-    require '1.8/ffi_c'
-  elsif RUBY_VERSION =~ /1.9/
-    require '1.9/ffi_c'
+if !defined?(RUBY_ENGINE) || RUBY_ENGINE == "ruby"
+  begin
+    if RUBY_VERSION =~ /1.8/
+      require '1.8/ffi_c'
+    elsif RUBY_VERSION =~ /1.9/
+      require '1.9/ffi_c'
+    end
+  rescue Exception
+    require 'ffi_c'
   end
-rescue Exception
-  require 'ffi_c'
-end
 
-require 'ffi/ffi'
+  require 'ffi/ffi'
+end
