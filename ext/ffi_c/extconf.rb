365a162a5e1fda91a3d55dd1ad663051e50bb89e
Charlie Savage <cfis@zerista.com>
2011-08-17 23:54:28 -0600

    Update code for checking for libffi on windows.  Mingw will use ffi while VC++ will use libffi.  Also don't add in GNU makefile when on windows.

--- ext/ffi_c/extconf.rb
+++ ext/ffi_c/extconf.rb
@@ -5,15 +5,17 @@ if !defined?(RUBY_ENGINE) || RUBY_ENGINE == "ruby"
   require 'rbconfig'
   dir_config("ffi_c")
   
-  unless RbConfig::CONFIG['host_os'] =~ /mswin32|mingw32/
-    if pkg_config("libffi") || find_header("ffi.h", "/usr/local/include")
-      
-      # We need at least ffi_call and ffi_prep_closure
-      libffi_ok = have_library("ffi", "ffi_call", [ "ffi.h" ]) && have_func("ffi_prep_closure")
-      
-      # Check if the raw api is available.
-      $defs << "-DHAVE_RAW_API" if have_func("ffi_raw_call") && have_func("ffi_prep_raw_closure")
-    end
+  if pkg_config("libffi") ||
+     have_header("ffi.h") ||
+     find_header("ffi.h", "/usr/local/include")
+
+    # We need at least ffi_call and ffi_prep_closure
+    libffi_ok = have_library("ffi", "ffi_call", [ "ffi.h" ]) ||
+                have_library("libffi", "ffi_call", [ "ffi.h" ])
+    libffi_ok &&= have_func("ffi_prep_closure")
+
+    # Check if the raw api is available.
+    $defs << "-DHAVE_RAW_API" if have_func("ffi_raw_call") && have_func("ffi_prep_raw_closure")
   end
   
   have_func('rb_thread_blocking_region')
@@ -24,7 +26,7 @@ if !defined?(RUBY_ENGINE) || RUBY_ENGINE == "ruby"
   $defs << "-DHAVE_EXTCONF_H" if $defs.empty? # needed so create_header works
   $defs << "-DUSE_INTERNAL_LIBFFI" unless libffi_ok
   $defs << "-DRUBY_1_9" if RUBY_VERSION >= "1.9.0"
-  
+
   create_header
   
   $CFLAGS << " -mwin32 " if RbConfig::CONFIG['host_os'] =~ /cygwin/
@@ -41,7 +43,7 @@ if !defined?(RUBY_ENGINE) || RUBY_ENGINE == "ruby"
         mf.puts "include ${srcdir}/libffi.darwin.mk"
       elsif RbConfig::CONFIG['host_os'].downcase =~ /bsd/
         mf.puts '.include "${srcdir}/libffi.bsd.mk"'
-      else
+      elsif RbConfig::CONFIG['host_os'] !~ /mswin32|mingw32/
         mf.puts "include ${srcdir}/libffi.mk"
       end
     end
