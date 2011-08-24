a9faaeed716f3c4cec8bdf757f3cd90902aa85d9
Wayne Meissner <wmeissner@gmail.com>
2011-08-25 08:54:25 +1000

    Turn off errno saving on LLLrV bench

--- bench/bench_LLLrV.rb
+++ bench/bench_LLLrV.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), "bench_helper"))
 module LibTest
   extend FFI::Library
   ffi_lib LIBTEST_PATH
-  attach_function :bench_s64s64s64_v, [ :long_long, :long_long, :long_long ], :void
+  attach_function :bench_s64s64s64_v, [ :long_long, :long_long, :long_long ], :void, :save_errno => false
 end
 
 
