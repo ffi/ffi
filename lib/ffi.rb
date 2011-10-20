98bd40587a4f0224fb9c2a48bd1e425839134ed1
NARUSE, Yui <naruse@airemix.jp>
2011-10-20 20:08:21 +0900

    Support Ruby 2.0.

--- lib/ffi.rb
+++ lib/ffi.rb
@@ -4,6 +4,8 @@ if !defined?(RUBY_ENGINE) || RUBY_ENGINE == "ruby"
       require '1.8/ffi_c'
     elsif RUBY_VERSION =~ /1.9/
       require '1.9/ffi_c'
+    else
+      require 'ffi_c'
     end
   rescue Exception
     require 'ffi_c'
