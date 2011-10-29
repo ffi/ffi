e23913dc5af4ee66442616b3d51caffedf65a6ec
Matijs van Zuijlen <matijs@matijs.net>
2011-10-29 21:49:06 +0200

    Avoid already initialized constant warnings.

--- spec/ffi/custom_type_spec.rb
+++ spec/ffi/custom_type_spec.rb
@@ -17,32 +17,28 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "spec_helper"))
 
 describe "functions with custom types" do
-  before :each do
-    
-    Custom_enum = Class.new do
-      extend FFI::DataConverter
-      ToNativeMap= { :a => 1, :b => 2, :c => 3 }
-      FromNativeMap = { 1 => :a, 2 => :b, 3 => :c }
+  class Custom_enum
+    extend FFI::DataConverter
+    ToNativeMap= { :a => 1, :b => 2, :c => 3 }
+    FromNativeMap = { 1 => :a, 2 => :b, 3 => :c }
 
-      def self.native_type
-        @native_type_called = true
-        FFI::Type::INT32
-      end
-
-      def self.to_native(val, ctx)
-        @to_native_called = true
-        ToNativeMap[val]
-      end
-
-      def self.from_native(val, ctx)
-        @from_native_called = true
-        FromNativeMap[val]
-      end
-      def self.native_type_called?; @native_type_called; end
-      def self.from_native_called?; @from_native_called; end
-      def self.to_native_called?; @to_native_called; end
+    def self.native_type
+      @native_type_called = true
+      FFI::Type::INT32
     end
 
+    def self.to_native(val, ctx)
+      @to_native_called = true
+      ToNativeMap[val]
+    end
+
+    def self.from_native(val, ctx)
+      @from_native_called = true
+      FromNativeMap[val]
+    end
+    def self.native_type_called?; @native_type_called; end
+    def self.from_native_called?; @from_native_called; end
+    def self.to_native_called?; @to_native_called; end
   end
 
   it "can attach with custom return type" do
