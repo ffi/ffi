70ca2f4fdd328275375838b488dbe354173ce2f3
Antonio Terceiro <terceiro@softwarelivre.org>
2011-12-25 21:51:18 -0200

    Add :remote option to FFI:TypesGenerator#generate
    This is useful for porting to other architecture for which you don't
    have ruby installed.

--- lib/ffi/tools/types_generator.rb
+++ lib/ffi/tools/types_generator.rb
@@ -51,7 +51,12 @@ module FFI
         C
 
         io.close
-        typedefs = `gcc -E -x c #{options[:cppflags]} -D_DARWIN_USE_64_BIT_INODE -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -c #{io.path}`
+        cmd = "gcc -E -x c #{options[:cppflags]} -D_DARWIN_USE_64_BIT_INODE -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -c"
+        if options[:remote]
+          typedefs = `ssh #{options[:remote]} #{cmd} - < #{io.path}`
+        else
+          typedefs = `#{cmd} #{io.path}`
+        end
       end
       
       code = ""
