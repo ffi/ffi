2243b240995b3b27e42b9a85f2e64cae8fc39418
Wayne Meissner <wmeissner@gmail.com>
2012-04-08 05:47:30 +1000

    Add a gettimeofday bench with explicit Buffer backed struct instead of MemoryPointer

--- bench/bench_gettimeofday.rb
+++ bench/bench_gettimeofday.rb
@@ -30,6 +30,12 @@ puts "Benchmark FFI gettimeofday(2) (Timeval.new(FFI::MemoryPointer.new), nil) p
     iter.times { Posix.gettimeofday(Timeval.new(FFI::MemoryPointer.new(Timeval)), nil) }
   }
 }
+puts "Benchmark FFI gettimeofday(2) (Timeval.new(FFI::Buffer.new), nil) performance, #{iter}x"
+10.times {
+  puts Benchmark.measure {
+    iter.times { Posix.gettimeofday(Timeval.new(FFI::Buffer.new(Timeval)), nil) }
+  }
+}
 puts "Benchmark FFI gettimeofday(2) (pre allocated pointer, nil) performance, #{iter}x"
 10.times {
   t = Timeval.new FFI::MemoryPointer.new(Timeval)
