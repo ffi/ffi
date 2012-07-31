3edbb57ba786e4d6744b59ee6082a08e20752672
Wayne Meissner <wmeissner@gmail.com>
2012-07-31 20:47:40 +1000

    Remove definition of FFI_STDCALL from Types.h

--- ext/ffi_c/Types.h
+++ ext/ffi_c/Types.h
@@ -69,10 +69,6 @@ typedef enum {
 #include <ffi.h>
 #include "Type.h"
 
-#ifndef FFI_STDCALL
-#define FFI_STDCALL FFI_DEFAULT_ABI
-#endif
-
 VALUE rbffi_NativeValue_ToRuby(Type* type, VALUE rbType, const void* ptr);
 void rbffi_Types_Init(VALUE moduleFFI);
 
