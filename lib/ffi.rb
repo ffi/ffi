71a78e8d7afad0c632ec436d0ae9f6f56fd5fba1
Wayne Meissner <wmeissner@gmail.com>
2013-03-10 18:40:11 +1000

    Try to load the 2.0 pre-compiled lib

--- lib/ffi.rb
+++ lib/ffi.rb
@@ -5,6 +5,8 @@ if !defined?(RUBY_ENGINE) || RUBY_ENGINE == 'ruby' || RUBY_ENGINE == 'rbx'
       require '1.8/ffi_c'
     elsif RUBY_VERSION =~ /1.9/
       require '1.9/ffi_c'
+    elsif RUBY_VERSION =~ /2.0/
+      require '2.0/ffi_c'
     else
       require 'ffi_c'
     end
