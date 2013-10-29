de4535bb1c5b9c0d60449fb16275aba7c0f84cd9
Charles Oliver Nutter <headius@headius.com>
2013-10-29 15:21:19 -0500

    Fix bad search and replace in C code.

--- ext/ffi_c/Type.h
+++ ext/ffi_c/Type.h
@@ -1,23 +1,8 @@
 /*
  * Copyright (c) 2009, Wayne Meissner
+ * Copyright (C) 2009 Luc Heinrich <luc@honk-honk.com>
  *
  * This file is part of ruby-ffi.
- *
- * This code is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License version 3 only, as
- * published by the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
- * version 3 for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * version 3 along with this work.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Parts of this file are derived from code under the following license.
- *
- * Copyright (C) 2009 Luc Heinrich <luc@honk-honk.com>
  *
  * All rights reserved.
  *
