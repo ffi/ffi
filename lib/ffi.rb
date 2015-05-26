83c53aa8d51a24f6f7e2f866eeb1649484f57a22
tduehr <tduehr@gmail.com>
2015-05-26 08:26:25 -0500

    fix library load... forever
    thanks @davispuh

--- lib/ffi.rb
+++ lib/ffi.rb
@@ -1,19 +1,7 @@
 if !defined?(RUBY_ENGINE) || RUBY_ENGINE == 'ruby' || RUBY_ENGINE == 'rbx'
   Object.send(:remove_const, :FFI) if defined?(::FFI)
   begin
-    if RUBY_VERSION =~ /^1\.8/
-      require '1.8/ffi_c'
-    elsif RUBY_VERSION =~ /^1\.9/
-      require '1.9/ffi_c'
-    elsif RUBY_VERSION =~ /^2\.0/
-      require '2.0/ffi_c'
-    elsif RUBY_VERSION =~ /^2\.1/
-      require '2.1/ffi_c'
-    elsif RUBY_VERSION =~ /^2\.2/
-      require '2.2/ffi_c'
-    else
-      require 'ffi_c'
-    end
+    require RUBY_VERSION.split('.')[0, 2].join('.') + '/ffi_c'
   rescue Exception
     require 'ffi_c'
   end
