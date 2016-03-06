52e4fe5afaed57d4cf02b1cec1ea5f15b619f9b4
Russell Currey <ruscur@russell.cc>
2016-03-07 10:50:20 +1100

    libffi: Disable building of docs
    The updated libffi requires "makeinfo" in order to build the docs, adding
    an unnecessary dependency.  Work around this by disabling the docs.
    
    Signed-off-by: Russell Currey <ruscur@russell.cc>

--- ext/ffi_c/libffi.mk
+++ ext/ffi_c/libffi.mk
@@ -8,6 +8,6 @@ $(LIBFFI):
 	    echo "Configuring libffi"; \
 	    cd "$(LIBFFI_BUILD_DIR)" && \
 		/usr/bin/env CFLAGS="$(LIBFFI_CFLAGS)" GREP_OPTIONS="" \
-		/bin/sh $(LIBFFI_CONFIGURE) $(LIBFFI_HOST) > /dev/null; \
+		/bin/sh $(LIBFFI_CONFIGURE) $(LIBFFI_HOST) --disable-docs > /dev/null; \
 	fi
 	$(MAKE) -C "$(LIBFFI_BUILD_DIR)"
