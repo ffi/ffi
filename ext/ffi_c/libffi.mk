a3e6f8345998411b0d7bbc5a41f9eb2ac52c6aec
Fredrik Fornwall <fredrik@fornwall.net>
2017-07-17 17:49:53 +0200

    Do not assume a path to the sh and env binaries (#528)
    This improves portability, specifically for non-rooted Android
    devices which lacks /bin and /usr folders.

--- ext/ffi_c/libffi.mk
+++ ext/ffi_c/libffi.mk
@@ -12,7 +12,7 @@ $(LIBFFI):
 	@if [ ! -f "$(LIBFFI_BUILD_DIR)"/Makefile ]; then \
 		echo "Configuring libffi"; \
 		cd "$(LIBFFI_BUILD_DIR)" && \
-		/usr/bin/env CFLAGS="$(LIBFFI_CFLAGS)" GREP_OPTIONS="" \
-		/bin/sh $(LIBFFI_CONFIGURE) $(LIBFFI_HOST) > /dev/null; \
+		env CFLAGS="$(LIBFFI_CFLAGS)" GREP_OPTIONS="" \
+		sh $(LIBFFI_CONFIGURE) $(LIBFFI_HOST) > /dev/null; \
 	fi
 	$(MAKE) -C "$(LIBFFI_BUILD_DIR)"
