ffi/ffi

commit ad53a3f8c419a0d3e42573419ee35d3e395bcd69

Fix compatibility with PPC64LE platform (#577)
* prepare for libffi submodule

remove libffi from tree

* return libffi

This time as a submodule.

* update dependency

* add autogen.sh to makefile stubs

libffi uses autoconf; a fresh checkout does not have a configure script

* don't build docs

* Bump libffi to current HEAD

* Fix HAVE_FFI_PREP_CIF_VAR define

If vendored library used `have_func('ffi_prep_cif_var')` call always
returns `false`. Because of that `ffi_prep_cif` was used which caused
many failures related to double/float on PPC64LE platform.

Fixes #413

* Mention submodule in README

* Fix gem install command in README

* Bump rake-compiler-dock

Use latest version with Ubuntu 17.04 base image

* Fix rake gem:windows command

configure.ac:187: error: possibly undefined macro: LT_SYS_SYMBOL_USCORE
      If this token and others are legitimate, please use m4_pattern_allow.
      See the Autoconf documentation.
autoreconf: /usr/bin/autoconf failed with exit status: 1

* Fix building 2.1/2.2 on macOS

* Unmask 2.3 on macOS

ext/ffi_c/libffi.darwin.mk
@@ -8,7 +8,7 @@ BUILD_DIR := $(shell pwd)
 INCFLAGS += -I"$(BUILD_DIR)"
 
 # Work out which arches we need to compile the lib for
-ARCHES := 
+ARCHES :=
 ARCHFLAGS ?= $(filter -arch %, $(CFLAGS))
 
 ifneq ($(findstring -arch ppc,$(ARCHFLAGS)),)
@@ -26,11 +26,16 @@ endif
 ifeq ($(strip $(ARCHES)),)
 LIBFFI_BUILD_DIR = $(BUILD_DIR)/libffi-$(arch)
 # Just build the one (default) architecture
-$(LIBFFI):		
+$(LIBFFI):
 	@mkdir -p "$(LIBFFI_BUILD_DIR)" "$(@D)"
+	@if [ ! -f "$(LIBFFI_BUILD_DIR)"/configure ]; then \
+		echo "Running autoreconf for libffi"; \
+		cd "$(LIBFFI_SRC_DIR)" && \
+		/bin/sh $(LIBFFI_AUTOGEN) > /dev/null; \
+	fi
 	@if [ ! -f "$(LIBFFI_BUILD_DIR)"/Makefile ]; then \
-	    echo "Configuring libffi"; \
-	    cd "$(LIBFFI_BUILD_DIR)" && \
+		echo "Configuring libffi"; \
+		cd "$(LIBFFI_BUILD_DIR)" && \
 		/usr/bin/env CC="$(CC)" LD="$(LD)" CFLAGS="$(LIBFFI_CFLAGS)" GREP_OPTIONS="" \
 		/bin/sh $(LIBFFI_CONFIGURE) $(LIBFFI_HOST) > /dev/null; \
 	fi
@@ -42,6 +47,11 @@ LIBTARGETS = $(foreach arch,$(ARCHES),"$(BUILD_DIR)"/libffi-$(arch)/.libs/libffi
 # Build a fat binary and assemble
 build_ffi = \
 	mkdir -p "$(BUILD_DIR)"/libffi-$(1); \
+	(if [ ! -f "$(BUILD_DIR)"/libffi-$(1)/configure ]; then \
+		echo "Running autoreconf for libffi"; \
+		cd "$(LIBFFI_SRC_DIR)" && \
+		/bin/sh $(LIBFFI_AUTOGEN) > /dev/null; \
+	fi); \
 	(if [ ! -f "$(BUILD_DIR)"/libffi-$(1)/Makefile ]; then \
 	    echo "Configuring libffi for $(1)"; \
 	    cd "$(BUILD_DIR)"/libffi-$(1) && \

commit c41bacef1e4b1c7bc51626172ffe19e9ceb4cc3d

Fix wrong path to search for configure
This triggered unnecessary rebuild of configure in darwin multi arch.
It stood out in issue #609 but is probably unrelated to the size_t issue there.

This issue was introduced in commit ad53a3f8c4 and was fixed for other
build targets in bfc330a3c.

ext/ffi_c/libffi.darwin.mk
@@ -47,7 +47,7 @@ LIBTARGETS = $(foreach arch,$(ARCHES),"$(BUILD_DIR)"/libffi-$(arch)/.libs/libffi
 # Build a fat binary and assemble
 build_ffi = \
 	mkdir -p "$(BUILD_DIR)"/libffi-$(1); \
-	(if [ ! -f "$(BUILD_DIR)"/libffi-$(1)/configure ]; then \
+	(if [ ! -f "$(LIBFFI_SRC_DIR)"/configure ]; then \
 		echo "Running autoreconf for libffi"; \
 		cd "$(LIBFFI_SRC_DIR)" && \
 		/bin/sh $(LIBFFI_AUTOGEN) > /dev/null; \