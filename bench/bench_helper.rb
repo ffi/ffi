36da05ab7e2eee3170f12257508a28c0fda44c46
Benoit Daloze <eregontp@gmail.com>
2019-02-01 22:45:14 +0100

    Fix bench_helper to use FFI from the repository and not the gem

--- bench/bench_helper.rb
+++ bench/bench_helper.rb
@@ -1,6 +1,8 @@
-require "rubygems"
-#require 'ffi/times' if RUBY_PLATFORM =~ /java/
 require 'benchmark'
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ffi'
-ITER = ENV['ITER'] ? ENV['ITER'].to_i : 100000
+
+ITER = ENV['ITER'] ? ENV['ITER'].to_i : 100_000
+
 LIBTEST_PATH = File.expand_path("../../spec/ffi/fixtures/libtest.#{FFI::Platform::LIBSUFFIX}", __FILE__)
