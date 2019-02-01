b7a7c3f37d92e40ccce1335d485736aea0f27b63
Benoit Daloze <eregontp@gmail.com>
2019-02-01 23:06:50 +0100

    Make sure the C extension is compiled before running benchmarks
    * Otherwise, the C extension from the gem might be picked instead.

--- bench/bench_helper.rb
+++ bench/bench_helper.rb
@@ -1,6 +1,13 @@
 require 'benchmark'
 
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+lib = File.expand_path('../../lib', __FILE__)
+
+cext = "#{lib}/ffi_c.so"
+unless File.exist?(cext)
+  abort "#{cext} is not compiled. Compile it with 'rake compile' first."
+end
+
+$LOAD_PATH.unshift(lib)
 require 'ffi'
 
 ITER = ENV['ITER'] ? ENV['ITER'].to_i : 100_000
