4d1c93cfeae2173f7aa38fbc4b2f2076e1642ac1
Benoit Daloze <eregontp@gmail.com>
2019-04-03 17:42:22 +0200

    Test using FFI::Function and unsigned values in callback_spec.rb

--- spec/ffi/callback_spec.rb
+++ spec/ffi/callback_spec.rb
@@ -500,28 +500,32 @@ describe "Callback with " do
     expect(v).to eq(-1)
   end
 
+  def testCallbackU8rV(value)
+    v1 = 0xdeadbeef
+    LibTest.testCallbackU8rV(value) { |i| v1 = i }
+    expect(v1).to eq(value)
+
+    # Using a FFI::Function (v2) should be consistent with the direct callback (v1)
+    v2 = 0xdeadbeef
+    fun = FFI::Function.new(:void, [:uchar]) { |i| v2 = i }
+    LibTest.testCallbackU8rV(fun, value)
+    expect(v2).to eq(value)
+  end
+
   it ":uchar (0) argument" do
-    v = 0xdeadbeef
-    LibTest.testCallbackU8rV(0) { |i| v = i }
-    expect(v).to eq(0)
+    testCallbackU8rV(0)
   end
 
   it ":uchar (127) argument" do
-    v = 0xdeadbeef
-    LibTest.testCallbackU8rV(127) { |i| v = i }
-    expect(v).to eq(127)
+    testCallbackU8rV(127)
   end
 
   it ":uchar (128) argument" do
-    v = 0xdeadbeef
-    LibTest.testCallbackU8rV(128) { |i| v = i }
-    expect(v).to eq(128)
+    testCallbackU8rV(128)
   end
 
   it ":uchar (255) argument" do
-    v = 0xdeadbeef
-    LibTest.testCallbackU8rV(255) { |i| v = i }
-    expect(v).to eq(255)
+    testCallbackU8rV(255)
   end
 
   it ":short (0) argument" do
