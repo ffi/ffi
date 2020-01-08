bd45ff5913757d67d96d81f24ef7a919dbdf25ce
Lars Kanis <kanis@comcard.de>
2020-01-08 13:28:34 +0100

    Namespace all benchmarks since they are loaded into one process
    This is a follow-up to the previous commit.
    The benchmarks also made use of the redefinition feature of struct layouts.
    Namespacing avoids redefinition, but uses separate classes for all structs.

--- bench/bench_SrV.rb
+++ bench/bench_SrV.rb
@@ -1,16 +1,18 @@
 require_relative 'bench_helper'
 
-module LibTest
-  extend FFI::Library
-  ffi_lib LIBTEST_PATH
-  attach_function :bench_S_v, [ :string ], :void
-end
+module BenchSrV
+  module LibTest
+    extend FFI::Library
+    ffi_lib LIBTEST_PATH
+    attach_function :bench_S_v, [ :string ], :void
+  end
 
-puts "Benchmark [ :string ], :void performance, #{ITER}x calls"
+  puts "Benchmark [ :string ], :void performance, #{ITER}x calls"
 
-s = 'a' * 1000
-10.times {
-  puts Benchmark.measure {
-    ITER.times { LibTest.bench_S_v(s) }
+  s = 'a' * 1000
+  10.times {
+    puts Benchmark.measure {
+      ITER.times { LibTest.bench_S_v(s) }
+    }
   }
-}
+end
--- bench/bench_getlogin.rb
+++ bench/bench_getlogin.rb
@@ -1,28 +1,30 @@
 require_relative 'bench_helper'
 require 'etc'
 
-iter = ITER
+module BenchGetlogin
+  iter = ITER
 
-module Posix
-  extend FFI::Library
-  ffi_lib FFI::Library::LIBC
-  attach_function :getlogin, [], :string
-end
-if Posix.getlogin != Etc.getlogin
-  raise ArgumentError, "FFI getlogin returned incorrect value"
-end
+  module Posix
+    extend FFI::Library
+    ffi_lib FFI::Library::LIBC
+    attach_function :getlogin, [], :string
+  end
+  if Posix.getlogin != Etc.getlogin
+    raise ArgumentError, "FFI getlogin returned incorrect value"
+  end
 
-puts "Benchmark FFI getlogin(2) performance, #{ITER}x"
+  puts "Benchmark FFI getlogin(2) performance, #{ITER}x"
 
-10.times {
-  puts Benchmark.measure {
-    iter.times { Posix.getlogin }
+  10.times {
+    puts Benchmark.measure {
+      iter.times { Posix.getlogin }
+    }
   }
-}
 
-puts "Benchmark Etc.getlogin performance, #{ITER}x"
-10.times {
-  puts Benchmark.measure {
-    iter.times { Etc.getlogin }
+  puts "Benchmark Etc.getlogin performance, #{ITER}x"
+  10.times {
+    puts Benchmark.measure {
+      iter.times { Etc.getlogin }
+    }
   }
-}
+end
